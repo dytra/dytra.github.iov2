5f2c30fb9875cfc0f36ac05729e36c2df7eebb80
dytra <vancielnugraha@gmail.com>
2023-02-10 17:09:49 +0700

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,14 +5,52 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
+  const menus = [
+    {
+      label: "HOME"
+    },
+    {
+      label: "ABOUT",
+    },
+    {
+      label: "MY WORK"
+    },
+    {
+      label: "CONTACT",
+      classes: "bg-black px-4 py-3 text-white rounded-md"
+    }
+  ];
   return (
     <html lang="en">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet" />
+      </head>
+      <body>
+        <main className="container mx-auto">
+          <nav className="flex pt-3 items-center">
+            <div className="logo">
+              <span>dytrafolio</span>
+            </div>
+            <div className="menu-container ml-auto">
+              <ul className="flex gap-5">
+                {menus.map(menu => {
+                  return (
+                    <li className="menu-item font-medium"><a href="" className={menu.classes}>{menu.label}</a></li>
+                  )
+                })}
+              </ul>
+            </div>
+          </nav>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,31 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+// import Image from 'next/image'
+// import { Inter } from '@next/font/google'
+// import styles from './page.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div className="spacer h-10"></div>
+      <div className="hero flex">
+        <div className="left flex-1">
+          <h1 className="text-5xl">Hi there! 👋</h1>
+          <div className="mt-3">
+            <div>
+              <p className="text-gray-400">I am a Frontend Web Developer with a passion for creating visually stunning, responsive websites. My expertise includes HTML, CSS, JavaScript, and React. I strive to improve my skills and deliver high-quality, user-friendly websites that meet client and user needs.</p>
+            </div>
+            <div>
+              <a href="#" className="bg-black text-white py-5">Connect With Me</a>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        <div className="right flex-1">
+          <span>test</span>
         </div>
+
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
